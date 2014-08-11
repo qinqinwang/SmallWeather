40b6a6c4dfdb8f158fcfc9a37a6b060cc5db32de
qinqinwang <1414091585@qq.com>
2014-08-11 09:19:58 +0800

    数据获取

--- src/com/weather/view/MainActivity.java
+++ src/com/weather/view/MainActivity.java
@@ -1,39 +1,115 @@
 package com.weather.view;
 
-import com.smallweather.R;
-import com.smallweather.R.id;
-import com.smallweather.R.layout;
-import com.smallweather.R.menu;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.ViewFlipper;
 
-public class MainActivity extends Activity {
+import com.smallweather.R;
+import com.weather.adapter.MyAdapter;
+import com.weather.util.HttpUtil;
+
+public class MainActivity extends Activity{
+	private ListView listView;
+	private TextView date;
+	private Button setting;
+	private String result;
+	private List<String> list = new ArrayList<String>();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		listView = (ListView) findViewById(R.id.listView);
+		listView.setAdapter(new MyAdapter(this, getData()));
+		date = (TextView) findViewById(R.id.date);
+		getDate();
+		setting = (Button) findViewById(R.id.setting);
+		setting.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent();
+				intent.setClass(MainActivity.this, SettingActivity.class);
+				startActivity(intent);
+				overridePendingTransition(R.anim.push_left_out,
+						R.anim.push_left_in);
+				MainActivity.this.finish();
+
+			}
+		});
+
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	private void getDate() {
+		// TODO Auto-generated method stub
+		Calendar calendar = Calendar.getInstance();
+		int month = calendar.get(Calendar.MONTH);
+		int day = calendar.get(Calendar.DATE);
+		date.setText(month + "." + day);
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+	private List<String> getData() {
+		// TODO Auto-generated method stub
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				HttpUtil httpUtil = new HttpUtil(MainActivity.this);
+				result = httpUtil
+						.getJsonContent("http://dev.365jinbi.com/weather/");
+				String json = result;
+				Log.v("wang", "   result  =" + result);
+				JSONArray jsonArr;
+				JSONObject obj;
+
+				try {
+					jsonArr = new JSONArray(json);
+					obj = (JSONObject) jsonArr.get(0);
+					list.add(obj.getString("city"));
+					list.add(obj.getString("type"));
+					list.add(obj.getString("temperature"));
+					list.add(obj.getString("wind_direction"));
+					list.add(obj.getString("wind_force"));
+					// for (int i = 0; i < jsonArr.length(); i++) {
+					// obj = (JSONObject) jsonArr.getssss(i);
+					// list.add(obj.getString("city"));
+					// list.add(obj.getString("type"));
+					// list.add(obj.getString("temperature"));
+					// list.add(obj.getString("wind_direction"));
+					// list.add(obj.getString("wind_force"));
+					// }
+
+				} catch (JSONException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+			}
+		}).start();
+
+		return list;
 	}
+
+	
 }
